0ae0b44ab9431512af6df570bfedcc3070fcfe1d
Altin <altinmorina2002@gmail.com>
2024-04-05 21:53:23 +0200

    Implement API CRUD operations functions as services - Add enviroment data for development

---
+++ src/api/api.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import { apiUrl } from '../config/environment'
+
+// Function to make a GET request
+export const get = async (endpoint) => {
+    try {
+        const response = await axios.get(`${apiUrl}/${endpoint}`)
+        return response.data
+    } catch (error) {
+        console.error('Error:', error)
+        throw error
+    }
+}
+
+// Function to make a POST request
+export const post = async (endpoint, data) => {
+    try {
+        const response = await axios.post(`${apiUrl}/${endpoint}`, data)
+        return response.data
+    } catch (error) {
+        console.error('Error:', error)
+        throw error
+    }
+}
+
+// Function to make a PUT request
+export const put = async (endpoint, data) => {
+    try {
+        const response = await axios.put(`${apiUrl}/${endpoint}`, data)
+        return response.data
+    } catch (error) {
+        console.error('Error:', error)
+        throw error
+    }
+}
+
+// Function to make a DELETE request
+export const del = async (endpoint) => {
+    try {
+        const response = await axios.delete(`${apiUrl}/${endpoint}`)
+        return response.data
+    } catch (error) {
+        console.error('Error:', error)
+        throw error
+    }
+}
---
+++ src/environments/enviroment.development.js
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    environmentName: 'local',
+    apiUrl: 'https://localhost:3000',
+    userName: 'admin',
+    email: 'admin@example.com',
+    password: 'admin123'
+};
