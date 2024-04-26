cd81756d4bae07b6aed9690ae799b9d38ec0a0fe
Altin <altinmorina2002@gmail.com>
2024-04-27 00:17:19 +0200

    Change localhost url and api return data

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
-import { apiUrl } from '../config/environment'
 
+import { environment } from '../environments/enviroment.development'
+const apiUrl = environment.apiUrl
 // Function to make a GET request
 export const get = async (endpoint) => {
     try {
         const response = await axios.get(`${apiUrl}/${endpoint}`)
-        return response.data
+        return response
     } catch (error) {
         console.error('Error:', error)
         throw error
